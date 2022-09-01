a7426214b75423e9c968a09eefd50250cf8fb228
Zachary Dionne <zachary.dionne.01@edu.cegeptr.qc.ca>
2022-09-01 14:42:06 -0400

    creation of migrations

---
+++ database/migrations/2022_09_01_180042_create_conducteurs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conducteurs', function (Blueprint $table) {
+            $table->id();
+            $table->string("prenom", 20);
+            $table->string("nom", 20);
+            $table->string("matricule", 6);
+            $table->string("email", 80);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conducteurs');
+    }
+};
