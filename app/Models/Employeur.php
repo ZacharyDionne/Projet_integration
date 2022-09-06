3d47f032e04eb03d9df3146e6ecd8f9ba6def731
Anthony Proulx <antho.333@hotmail.fr>
2022-09-06 16:37:46 -0400

    Employeur

---
+++ app/Models/Employeur.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employeur extends Model
+{
+    use HasFactory;
+
+    public function up()
+    {
+        Schema::create("employeurs", function (Blueprint $table) {
+            $table->id();
+            $table->string("prenom", 20);
+            $table->string("nom", 20);
+            $table->string("adresseCourriel", 80);
+            $table->text("motDePasse");
+            $table->timestamp();
+        });  
+    }
+}
