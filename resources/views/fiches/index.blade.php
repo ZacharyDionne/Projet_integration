0aa24a451d377f2e358a7495da7b2e4f91e2befc
Raphaël Bacon <RaphBacon@gmail.com>
2022-09-08 13:04:23 -0400

    Page fiche - Calendrier ajouté

--- resources/views/fiches/index.blade.php
+++ resources/views/fiches/index.blade.php
@@ -1,11 +1,129 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>fiches.index</h1>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Fiche</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+<!--===============================================================================================-->
+	<link rel="stylesheet" href="fonts\font-awesome-4.7.0\css\font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" href="css/styleCalendar.css">
+<!--===============================================================================================-->
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Fiche - {Nom}</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="elegant-calencar d-md-flex">
+						<div class="wrap-header d-flex align-items-center img" style="background-image: url(images/bgCalendar.jpg);">
+				      <p id="reset">Aujourd'hui</p>
+			        <div id="header" class="p-0">
+								<!-- <div class="pre-button d-flex align-items-center justify-content-center"><i class="fa fa-chevron-left"></i></div> -->
+		            <div class="head-info">
+		            	<div class="head-month"></div>
+		                <div class="head-day"></div>
+		            </div>
+		            <!-- <div class="next-button d-flex align-items-center justify-content-center"><i class="fa fa-chevron-right"></i></div> -->
+			        </div>
+			      </div>
+			      <div class="calendar-wrap">
+			      	<div class="w-100 button-wrap">
+				      	<div class="pre-button d-flex align-items-center justify-content-center"><i class="fa fa-chevron-left"></i></div>
+				      	<div class="next-button d-flex align-items-center justify-content-center"><i class="fa fa-chevron-right"></i></div>
+			      	</div>
+			        <table id="calendar">
+		            <thead>
+		                <tr>
+	                    <th>Dim</th>
+	                    <th>Lun</th>
+	                    <th>Mar</th>
+	                    <th>Mer</th>
+	                    <th>Jeu</th>
+	                    <th>Ven</th>
+	                    <th>Sam</th>
+		                </tr>
+		            </thead>
+		            <tbody>
+	                <tr>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                </tr>
+	                <tr>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                </tr>
+	                <tr>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                </tr>
+	                <tr>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                </tr>
+	                <tr>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                </tr>
+	                <tr>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                  <td></td>
+	                </tr>
+		            </tbody>
+			        </table>
+			      </div>
+			    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="js/mainCalendar.js"></script>
+
+	</body>
 </html>
+
