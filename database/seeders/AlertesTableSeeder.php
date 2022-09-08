11d122b0736fd03d3bee448f8e0f6ca1b1789949
Zachary Dionne <zachary.dionne.01@edu.cegeptr.qc.ca>
2022-09-08 13:01:38 -0400

    seed

---
+++ database/seeders/AlertesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class AlertesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("alertes")->insert(
+            [
+                [
+                    
+                ]
+
+
+
+
+
+
+            ]
+        );
+    }
+}
