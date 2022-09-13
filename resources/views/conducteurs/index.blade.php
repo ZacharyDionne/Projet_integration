8fe59889850df3482c3e518ca327f6a24c1b752c
JM <jade.morin2001@outlook.com>
2022-09-13 16:31:45 -0400

    vues

--- resources/views/conducteurs/index.blade.php
+++ resources/views/conducteurs/index.blade.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     <h1>conduteurs.index</h1>
+    
+    <h3>Voici la liste des conducteurs!</h3>
+    @if (count($conducteurs))
+        @foreach($conducteurs as $conducteur)
+
+        <ul>
+            <li>{{ $conducteur->id }}</li>
+            <li>{{ $conducteur->prenom }}, {{ $conducteur->nom }}</li>
+            <li>{{ $conducteur->matricule }}</li>
+            <li>{{ $conducteur->adresseCourriel }}</li>
+            <li>{{ $conducteur->motDePasse }}</li>
+            <li>{{ $conducteur->actif }}</li>
+        </ul>
+
+        @endforeach
+    @else
+        <p>Il n'y a aucun conducteur.</p>
+
+    @endif
+
 </body>
 </html>
--- resources/views/employeurs/index.blade.php
+++ resources/views/employeurs/index.blade.php
@@ -7,5 +7,7 @@
 </head>
 <body>
     <h1>employeurs.index</h1>
+    <h3>Voici la liste des employeurs</h3>
+
 </body>
 </html>
