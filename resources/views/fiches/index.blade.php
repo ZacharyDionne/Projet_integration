e1d1e2220c409d79c0845cd6f3073d7d48b4fec4
Raphaël Bacon <RaphBacon@gmail.com>
2022-09-13 17:05:57 -0400

    Calendrier pop-up Back-end

--- resources/views/fiches/index.blade.php
+++ resources/views/fiches/index.blade.php
@@ -27,7 +27,7 @@
 			<div class="row">
 				<div class="col-md-12">
 					<div class="elegant-calencar d-md-flex">
-						<div class="wrap-header d-flex align-items-center img" style="background-image: url(images/bgCalendar.jpg);">
+						<div class="wrap-header d-flex align-items-center img" style="background-image: url(images/bgCalendar.png);">
 				      <p id="reset">Aujourd'hui</p>
 			        <div id="header" class="p-0">
 								<!-- <div class="pre-button d-flex align-items-center justify-content-center"><i class="fa fa-chevron-left"></i></div> -->
@@ -57,7 +57,7 @@
 		            </thead>
 		            <tbody>
 	                <tr>
-	                  <td></td>
+	                  <td data-toggle="modal" data-target="#exampleModalCenter"></td>
 	                  <td></td>
 	                  <td></td>
 	                  <td></td>
@@ -118,6 +118,49 @@
 			</div>
 		</div>
 	</section>
+
+	    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content rounded-0">
+          <div class="modal-body py-0">
+
+            
+            <div class="d-flex main-content">
+              <div class="bg-image promo-img mr-3" style="background-image: url('images/img_1.jpg');">
+              </div>
+              <div class="content-text p-4">
+                <h3>Sign up to access all the resourcess</h3>
+                <p>All their equipment and instruments are alive. The sky was cloudless and of a deep dark blue.</p>
+				<!--  -->
+
+                <form action="#">
+                  <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" class="form-control" id="email">
+                  </div>
+                  <div class="form-group">
+                    <label for="name">First Name</label>
+                    <input type="text" class="form-control" id="name">
+                  </div>
+                  <div class="form-group">
+                    <label for="name">Password</label>
+                    <input type="password" class="form-control" id="name">
+                  </div>
+                  <div class="form-group">
+                    <input type="submit" value="Sign up" class="btn btn-primary btn-block">
+                  </div>
+                  <div class="form-group ">
+                    <p class="custom-note"><small>By signing up you will agree to our <a href="#">Privacy Policy</a></small></p>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
 
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
   <script src="vendor/bootstrap/js/popper.js"></script>
