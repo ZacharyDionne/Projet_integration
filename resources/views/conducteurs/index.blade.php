39cd8cacc19746377ef7a17d66d3bd9ecd4689cb
Anthony Proulx <antho.333@hotmail.fr>
2022-09-22 11:13:05 -0400

    FrontEnd

--- resources/views/conducteurs/index.blade.php
+++ resources/views/conducteurs/index.blade.php
@@ -1,9 +1,30 @@
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conducteurs</title>
+<!--===============================================================================================-->	
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
     <h1>conduteurs.index</h1>
@@ -12,13 +33,13 @@
     @if (count($conducteurs))
         @foreach($conducteurs as $conducteur)
 
-        <ul>
-            <li>{{ $conducteur->id }}</li>
-            <li>{{ $conducteur->prenom }}, {{ $conducteur->nom }}</li>
-            <li>{{ $conducteur->matricule }}</li>
-            <li>{{ $conducteur->adresseCourriel }}</li>
-            <li>{{ $conducteur->motDePasse }}</li>
-            <li>{{ $conducteur->actif }}</li>
+        <ul class="list-group list-group-horizontal">
+            <li class="list-group-item">{{ $conducteur->id }}</li>
+            <li class="list-group-item">{{ $conducteur->prenom }}, {{ $conducteur->nom }}</li>
+            <li class="list-group-item">{{ $conducteur->matricule }}</li>
+            <li class="list-group-item">{{ $conducteur->adresseCourriel }}</li>
+            <li class="list-group-item">{{ $conducteur->motDePasse }}</li>
+            <li class="list-group-item">{{ $conducteur->actif }}</li>
         </ul>
 
         @endforeach
@@ -27,5 +48,23 @@
 
     @endif
 
+<!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
+
