ccae9b7bbe45af9c52a3f41e3922b4f65c6f9d5a
Raphaël Bacon <RaphBacon@gmail.com>
2022-09-27 16:03:44 -0400

    Page confucteur

--- resources/views/conducteurs/index.blade.php
+++ resources/views/conducteurs/index.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -32,6 +33,131 @@
     <h3>Voici la liste des conducteurs!</h3>
     <a class="btn btn-default" href="{{ route('conducteurs.create') }}">Ajouter</a> 
 
+=======
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Table 02</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" href="css/styleTable.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" href="css/style.css">
+<!--===============================================================================================-->
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Table #02</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="table-wrap">
+						<table class="table">
+						  <thead class="bg-tr-up">
+						    <tr>
+						      <th>ID no.</th>
+						      <th>First Name</th>
+						      <th>Last Name</th>
+						      <th>Email</th>
+						      <th>&nbsp;</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						    <tr class="alert" role="alert">
+						      <th scope="row">001</th>
+						      <td>Mark</td>
+						      <td>Otto</td>
+						      <td>markotto@email.com</td>
+						      <td>
+						      	<a href="#" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</a>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						      <th scope="row">002</th>
+						      <td>Jacob</td>
+						      <td>Thornton</td>
+						      <td>jacobthornton@email.com</td>
+						      <td>
+						      	<a href="#" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</a>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						      <th scope="row">003</th>
+						      <td>Larry</td>
+						      <td>the Bird</td>
+						      <td>larrybird@email.com</td>
+						      <td>
+						      	<a href="#" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</a>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						      <th scope="row">004</th>
+						      <td>John</td>
+						      <td>Doe</td>
+						      <td>johndoe@email.com</td>
+						      <td>
+						      	<a href="#" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</a>
+				        	</td>
+						    </tr>
+						    <tr class="alert" role="alert">
+						      <th scope="row">005</th>
+						      <td>Gary</td>
+						      <td>Bird</td>
+						      <td>garybird@email.com</td>
+						      <td>
+						      	<a href="#" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</a>
+				        	</td>
+						    </tr>
+						  </tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+    <script src="js/mainTable.js"></script>
+<!--===============================================================================================-->
+
+	</body>
+</html>
+
+
+    
+    <!-- <h3>Voici la liste des conducteurs!</h3>
+>>>>>>> Stashed changes
     @if (count($conducteurs))
         @foreach($conducteurs as $conducteur)
 
@@ -48,6 +174,7 @@
     @else
         <p>Il n'y a aucun conducteur.</p>
 
+<<<<<<< Updated upstream
     @endif
 
 <!--===============================================================================================-->
@@ -70,3 +197,6 @@
 </body>
 </html>
 
+=======
+    @endif -->
+>>>>>>> Stashed changes
