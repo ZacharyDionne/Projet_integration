6702e781f38311dd44672a158cf2f91900651033
Anthony Proulx <antho.333@hotmail.fr>
2022-10-27 10:20:01 -0400

    Form better

--- resources/views/conducteurs/create.blade.php
+++ resources/views/conducteurs/create.blade.php
@@ -41,84 +41,95 @@
                     Créer un conducteur
 					</span>
 
-                    <div class="container-row-create">
+                    <div class="container-row-create-c3 p-t-100">
                         <!-- Matricule -->
                         <div>
-                            <div class="font-rg m-l-20">
+                            <div class="header-font font-rg m-l-20">
                                 <label for="matricule">Matricule</label>
                             </div>
-                            <div class="wrap-input100 validate-input m-b-16" data-validate="Veuillez saisir votre matricule">
-                                <input class="input100" id="matricule" type="text" name="matriculeConducteur" placeholder="Matricule" value="{{ old('matricule') }}">
+                            <div class="wrap-input100 validate-input" data-validate="Veuillez saisir votre matricule">
+                                <input class="input100" id="matricule" type="text" name="matriculeConducteur" placeholder="Insérer votre matricule" value="{{ old('matricule') }}">
                                 <span class="focus-input100"></span>
                             </div>
                         </div>
                         <!-- Prénom conducteur -->
                         <div>
-                            <div class="font-rg m-l-20">
+                            <div class="header-font font-rg m-l-20">
                                 <label for="prenom">Prénom</label>
                             </div>
-                            <div class="wrap-input100 validate-input m-b-16" data-validate="Saisissez un prénom">
-                                <input class="input100" id="prenom" type="text" name="prenomConducteur" placeholder="Prénom" value="{{ old('prenom') }}">
+                            <div class="wrap-input100 validate-input" data-validate="Saisissez un prénom">
+                                <input class="input100" id="prenom" type="text" name="prenomConducteur" placeholder="Insérer votre prénom" value="{{ old('prenom') }}">
                                 <span class="focus-input100"></span>
                             </div>
                         </div>
                         <!-- Nom conducteur-->
                         <div>
-                            <div class="font-rg m-l-20">
+                            <div class="header-font font-rg m-l-20">
                                 <label for="nom">Nom</label>
                             </div>
-                            <div class="wrap-input100 validate-input m-b-16" data-validate="Saisissez un nom">
-                                <input class="input100" id="nom" type="text" name="nomConducteur" placeholder="Nom" value="{{ old('nom') }}">
+                            <div class="wrap-input100 validate-input" data-validate="Saisissez un nom">
+                                <input class="input100" id="nom" type="text" name="nomConducteur" placeholder="Insérer votre nom" value="{{ old('nom') }}">
                                 <span class="focus-input100"></span>
                             </div>
                         </div>
+                    </div>
+                    <div class="container-row-create-c2 p-t-10">
                         <!-- Email -->
                         <div>
-                            <div class="font-rg m-l-20">
-                                <label for="email">Email</label>
+                            <div class="header-font font-rg m-l-20">
+                                <label for="email">Adresse électronique</label>
                             </div>
-                            <div class="wrap-input100 validate-input m-b-16" data-validate="Saisissez une adresse courriel">
-                                <input class="input100" id="email" type="email" name="adresseCourriel" placeholder="Adresse courriel" value="{{ old('adresseCourriel') }}">
+                            <div class="w-100 wrap-input100 validate-input" data-validate="Saisissez une adresse électronique">
+                                <input class="input100" id="email" type="email" name="adresseCourriel" placeholder="Insérer votre adresse électronique" value="{{ old('adresseCourriel') }}">
                                 <span class="focus-input100"></span>
                             </div>
                         </div>
+                        <!-- Statut -->
+                        <div>
+                            <div class="header-font font-rg m-l-16">
+                                <label for="email">Statut</label>
+                            </div>
+                            <!-- Actif -->              
+                            <div class="wrap-input100 validate-input">
+                                <input type="radio" id="actif" name="actifConducteur" value="1" checked="Yes">
+                                <label for="actif">Actif</label>
+                            </div>
+                            <!-- Inactif -->
+                            <div class="wrap-input100 validate-input">
+                                <input type="radio" id="inactif" name="actifConducteur" value="0">
+                                <label for="inactif">Inactif</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-row-create-c2">
                         <!-- Mot de passe -->
                         <div>
-                            <div class="font-rg m-l-20">
+                            <div class="header-font font-rg m-l-20">
                                 <label for="motdepasse">Mot de passe</label>
                             </div>
-                            <div class="wrap-input100 validate-input m-b-16" data-validate="Veuillez saisir votre mot de passe" value="{{ old('motDePasse') }}">
-                                <input class="input100" id="motdepasse" type="password" name="motDePasse" placeholder="Mot de passe">
+                            <div class="w-100 wrap-input100 validate-input" data-validate="Veuillez saisir votre mot de passe" value="{{ old('motDePasse') }}">
+                                <input class="input100" id="motdepasse" type="password" name="motDePasse" placeholder="Insérer votre mot de passe">
                                 <span class="focus-input100"></span>
                             </div>
                         </div>
                         <!-- Mot de passe vérification -->
                         <div>
-                            <div class="font-rg m-l-20">
+                            <div class="header-font font-rg m-l-20">
                                 <label for="vefMotdepasse">Vérification du mot de passe</label>
                             </div>
-                            <div class="wrap-input100 validate-input m-b-16" data-validate="Veuillez vérifier votre mot de passe">
-                                <input class="input100" id="vefMotdepasse" type="password" name="motDePasseVef" placeholder="Vérification" value="{{ old('motDePasse') }}">
+                            <div class="w-100 wrap-input100 validate-input" data-validate="Veuillez vérifier votre mot de passe">
+                                <input class="input100" id="vefMotdepasse" type="password" name="motDePasseVef" placeholder="Vérifier votre mot de passe" value="{{ old('motDePasse') }}">
                                 <span class="focus-input100"></span>
                             </div>
                         </div>
-                        <!-- Actif -->
-                        <div class="wrap-input100 validate-input">
-                            <input type="radio" id="actif" name="actifConducteur" value="1" checked="Yes">
-                            <label for="actif">Actif</label>
-                        </div>
-                        <!-- Inactif -->
-                        <div class="wrap-input100 validate-input">
-                            <input type="radio" id="inactif" name="actifConducteur" value="0">
-                            <label for="inactif">Inactif</label>
-                        </div>
                     </div>
 
-					<div class="container-login100-form-btn p-t-16 p-b-23">
-						<button class="login100-form-btn">
-							Créer
-						</button>
-					</div>
+                        <div class="container-login100-form-btn p-t-16 p-b-23">
+                            <button class="login100-form-btn">
+                                Créer
+                            </button>
+                        </div>
+                    </div>
 				</form>
 			</div>
 		</div>
