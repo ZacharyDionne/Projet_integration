4abdb333e168d2dcda5dbd35cd61bd25954d89f2
Zachary Dionne <zachary.dionne.01@edu.cegeptr.qc.ca>
2022-11-14 10:45:26 -0500

    Correction du lien de navigation

--- resources/views/includes/banniere.blade.php
+++ resources/views/includes/banniere.blade.php
@@ -1,20 +1,33 @@
 <nav class="navbar navbar-dark bg-tr topnav" id="myTopnav">
-    <a class="navbar-brand font-tr" href="{{ route('fiches.index') }}">
-        <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center" alt="">
-        Voir vos fiches
-    </a>
+    @if (auth()->user())
+        <a class="navbar-brand font-tr" href="{{ route('fiches.index') }}">
+            <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center">
+            Voir vos fiches
+        </a>
+    @elseif (auth()->guard('employe')->user()->type_id === 1)
+        <a class="navbar-brand font-tr" href="{{ route('conducteurs.index') }}">
+            <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center">
+            Conducteurs
+        </a>
+    @elseif (auth()->guard('employe')->user()->type_id === 2)
+        <a class="navbar-brand font-tr" href="{{ route('employes.index') }}">
+            <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center">
+            Employés
+        </a>
+    @endif
             
-    <a class="font-tr" href="
+    <!------ À ENLEVER ÉVENTUELLEMENT -------
+        <a class="font-tr" href="
         @if (auth()->user())
             {{ route('conducteurs.edit', [auth()->user()->id]) }}
         @else
             {{ route('employes.edit', [auth()->guard('employe')->user()->id]) }}
         @endif
     ">
-        <!-- <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center" alt=""> -->
+         <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center" alt="">
         <i class="fa fa-cog" width="50" height="50" class="d-inline-block align-center" aria-hidden="true"></i>
         paramètres
-    </a>
+    </a> -->
 
 
     <a class="font-tr btnDeco" href="{{ route('connexion.logout') }}">
