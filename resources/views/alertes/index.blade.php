b2f4c4ba2e748af6e10233acd64f39f5fad35e13
Raphaël Bacon <RaphBacon@gmail.com>
2022-11-29 17:49:18 -0500

    Baniere

--- resources/views/alertes/index.blade.php
+++ resources/views/alertes/index.blade.php
@@ -1,25 +1,31 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Voici les alertes!</h1>
-    @if (count($alertes))
-        <ul>
-        @foreach ($alertes as $alerte)
-            <li>id: $alerte->id</li>
-            <li>date: $alerte->date</li>
-            <li>état du compte: $alerte->active</li>
-            <li>message: $alerte->message</li>
-            <li>redirigé à: $alerte->idEmployeur</li>
-        @endforeach
-        </ul>
-    @else
-        <p>Il n'y a aucune alerte.</p>
-    @endif
-</body>
-</html>
+@php
+setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
+@endphp
+@extends('layouts.app')
+
+@section('titre', 'Fiches')
+
+@section('cssSupplementaire')
+<link rel="stylesheet" href="{{ asset('css/styleCalendar.css') }}">
+<link rel="stylesheet" href="{{ asset('css/styleModal.css') }}">
+<link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}" />
+
+<script defer src="{{ asset('js/mainCalendar.js') }}"></script>
+@endsection
+
+@section('contenu')
+<h1>Voici les alertes!</h1>
+@if (count($alertes))
+<ul>
+    @foreach ($alertes as $alerte)
+    <li>id: $alerte->id</li>
+    <li>date: $alerte->date</li>
+    <li>état du compte: $alerte->active</li>
+    <li>message: $alerte->message</li>
+    <li>redirigé à: $alerte->idEmployeur</li>
+    @endforeach
+</ul>
+@else
+<p>Il n'y a aucune alerte.</p>
+@endif
+@endsection
--- resources/views/includes/banniere.blade.php
+++ resources/views/includes/banniere.blade.php
@@ -2,46 +2,56 @@
     @php
     try
     {
-        $conducteur = auth()->user();
-        $employe = auth()->guard('employe')->user();
+    $conducteur = auth()->user();
+    $employe = auth()->guard('employe')->user();
     }
     catch (Throwable $e)
     {
-        $conducteur = null;
-        $employe = null;
+    $conducteur = null;
+    $employe = null;
     }
     @endphp
+    <img src="{{ asset('images/logo_BLANC.png') }}" width="48" height="48" class="banLogo displayBig align-center">
     @if ($conducteur)
-    <a class="noMargin navbar-brand font-tr" href="{{ route('fiches.index', $conducteur->id) }}">
-        <img src="{{ asset('images/logo_BLANC.png') }}" width="50" height="50" class="displayBig align-center">
-        <i class="fa fa-calendar displaySmall"></i>
-        <span>Voir vos fiches</span>
+    <a class="topnavButton noMargin navbar-brand font-tr align-middle" href="{{ route('fiches.index', $conducteur->id) }}">
+        <div class="text-nav">
+            <i class="fa fa-calendar fa-fw align-items-center"></i>
+            <span>Voir vos fiches</span>
+        </div>
     </a>
     @elseif ($employe)
     @if ($employe->type_id === 1)
-    <a class="noMargin navbar-brand font-tr" href="{{ route('conducteurs.index') }}">
-        <img src="{{ asset('images\logo_BLANC.png') }}" width="50" height="50" class="d-inline-block align-center">
-        Conducteurs
+    <a class="topnavButton noMargin navbar-brand font-tr" href="{{ route('conducteurs.index') }}">
+        <div class="text-nav">
+            <i class="fa fa-user fa-fw align-items-center"></i>
+            <span>Conducteurs</span>
+        </div>
     </a>
     @elseif ($employe->type_id === 2)
-    <a class="noMargin navbar-brand font-tr" href="{{ route('employes.index') }}">
-        <img src="{{ asset('images\logo_BLANC.png') }}" width="50" height="50" class="d-inline-block align-center">
-        Employés
+    <a class="topnavButton noMargin navbar-brand font-tr" href="{{ route('employes.index') }}">
+        <div class="text-nav">
+            <i class="fa fa-user fa-fw align-items-center"></i>
+            <span>Employés</span>
+        </div>
     </a>
     @endif
     @else
     <h1 class="navbar-brand font-tr">Une erreur est survenue. Si le problème persiste, veuillez contacter votre responsable.</h1>
     @endif
 
-    <a class="noMargin navbar-brand font-tr" href="{{ route('alertes.index') }}">
-        <i class="fa fa-bell" width="50" height="50" class="d-inline-block align-center" aria-hidden="true"></i>
-        <span>Alertes</span>
+    <a class="btnAlerte topnavButton noMargin navbar-brand font-tr" href="{{ route('alertes.index') }}">
+        <div class="text-nav">
+            <i class="fa fa-bell fa-fw" width="50" height="50" class="d-inline-block align-center" aria-hidden="true"></i>
+            <span>Notifications</span>
+        </div>
     </a>
 
     <a class="font-tr btnDeco" href="{{ route('connexion.logout') }}">
-        <!-- <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center" alt=""> -->
-        <i class="fa fa-sign-out" width="50" height="50" class="d-inline-block align-center" aria-hidden="true"></i>
-        <span>Déconnexion</span>
+        <div class="text-nav">
+            <!-- <img src="images\logo_BLANC.png" width="50" height="50" class="d-inline-block align-center" alt=""> -->
+            <i class="fa fa-sign-out fa-fw" width="50" height="50" class="d-inline-block align-center" aria-hidden="true"></i>
+            <span>Déconnexion</span>
+        </div>
     </a>
 
 </nav>
