53ce5dffd8791f932129918e1efb917332127d5d
Anthony Proulx <antho.333@hotmail.fr>
2022-12-08 10:12:39 -0500

    Few tweaks

--- resources/views/fiches/edit.blade.php
+++ resources/views/fiches/edit.blade.php
@@ -12,7 +12,7 @@ setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
 @endsection
 
 @section('contenu')
-    <section class="ftco-section p-3">
+    <section class="paddingCard">
 		<div class="flex-container">
             <div class="card card-employes">
                 <div class="card-header cardheader-employes">
@@ -25,23 +25,23 @@ setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
 				</div>
                 <div class="d-flex justify-content-around p-4">
                     <a class="btn btn-primary button-page font-tr w-100 mr-2" href="{{ route('fiches.edit', ['id' => $fiche->conducteur_id, 'date' => \Carbon\Carbon::parse($fiche->date)->subDay()->format('Y-m-d')]) }}">
-                        <i class="fa fa-arrow-left" aria-hidden="true"></i><span class="item-label"> Journée précédente</span>
+                        <i class="fa fa-arrow-left fa-fw" aria-hidden="true"></i><span class="item-label">Journée précédente</span>
                     </a>
                     <a class="btn btn-primary button-list font-tr w-100 ml-2 mr-2" href="{{ route('fiches.index', ['id' => $fiche->conducteur_id]) }}">
-                        <i class="fa fa-list" aria-hidden="true"></i><span class="item-label"> Retour à la liste des fiches</span>
+                        <i class="fa fa-list fa-fw" aria-hidden="true"></i><span class="item-label">Retour à la liste des fiches</span>
                     </a>
                     <a class="btn btn-primary button-page font-tr w-100 ml-2" href="{{ route('fiches.edit', ['id' => $fiche->conducteur_id, 'date' => \Carbon\Carbon::parse($fiche->date)->addDay()->format('Y-m-d')]) }}">
-                        <span class="item-label">Journée suivante </span><i class="fa fa-arrow-right" aria-hidden="true"></i>
+                        <span class="item-label">Journée suivante</span><i class="fa fa-arrow-right fa-fw" aria-hidden="true"></i>
                     </a>
                 </div>
 
-                <div class="grid-placement ml-4 mr-4">
+                <div class="grid-placement marginGrid">
                     <div class="d-flex justify-content-around">
                         <a class="btn btn-primary button-add font-tr w-100 mr-2" id="boutonAjouter">
-                            <i class="fa fa-plus" aria-hidden="true"></i><span class="item-label"> Ajouter</span>
+                            <i class="fa fa-plus fa-fw" aria-hidden="true"></i><span class="item-label">Ajouter</span>
                         </a>
                         <a class="btn btn-primary button-delete font-tr w-100 ml-2" id="boutonSupprimer">
-                            <i class="fa fa-trash" aria-hidden="true"></i><span class="item-label"> Supprimer</span>
+                            <i class="fa fa-trash fa-fw" aria-hidden="true"></i><span class="item-label">Supprimer</span>
                         </a>
                     </div>
 					<div class="row">
@@ -60,7 +60,7 @@ setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
 									</thead>
 									<tbody>
                                         @for ($i = 0; $i < count($plagesDeTemps); $i++)
-											<tr class="shadow-sm">
+											<tr class="shadow-sm colorTableContent">
                                                 <td class="font-rg">
                                                     <input type="checkbox" class="select">
                                                 </td>
@@ -90,7 +90,7 @@ setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
                 <form method="post" id="formModification" action="{{ route('fiches.update', [$fiche->conducteur_id]) }}">
                     @csrf
                     @method('patch')
-                    <div class="grid-placement mr-4 ml-4 mt-4">
+                    <div class="grid-placement marginGrid mt-4">
                         <h6 class="heading-sectionsmall text-left list-title float-left">Observation</h6>
                         <input type="hidden" id="plagesDeTemps" name="plagesDeTemps">
                         <input type="hidden" name="fiche_id" value="{{ $fiche->id }}">
@@ -99,10 +99,10 @@ setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8', 'fr-FR', 'fra');
                     </div>
                     <div class="d-flex justify-content-around p-4">
                         <button type="button" class="btn btn-primary button-page font-tr w-100 mr-2" id="boutonEnregistrer">
-                            <i class="fa fa-floppy-o" aria-hidden="true"></i><span class="item-label"> Enregistrer</span>
+                            <i class="fa fa-floppy-o fa-fw" aria-hidden="true"></i><span class="item-label">Enregistrer</span>
                         </button>
                         <button type="button" class="btn btn-primary button-list font-tr w-50 ml-2" id="boutonTerminer">
-                            <i class="fa fa-paper-plane" aria-hidden="true"></i><span class="item-label"> Terminer</span>
+                            <i class="fa fa-paper-plane fa-fw" aria-hidden="true"></i><span class="item-label">Completer</span>
                         </button>
                     </div>
                 </form>
