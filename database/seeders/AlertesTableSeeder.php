8cd38f98ff4fd099b98d795c39b4e80e2e45802a
Raphaël Bacon <RaphBacon@gmail.com>
2022-12-13 15:40:57 -0500

    Alerte 10h

--- database/seeders/AlertesTableSeeder.php
+++ database/seeders/AlertesTableSeeder.php
@@ -16,12 +16,33 @@ class AlertesTableSeeder extends Seeder
     public function run()
     {
         DB::table('alertes')->insert([
-            'type' => '2',
-            'conducteur_id' => 1,
-            'message' => 'La fiche du dimanche, 4 décembre 2022 n\'est pas complète. Veuillez la compléter.',
-            'actif' => '1',
-            'date' => '2021-01-01',
-            'idEmploye' => 1,
+            [
+                'type' => '2',
+                'conducteur_id' => 1,
+                'message' => 'La fiche du dimanche, 4 décembre 2022 n\'est pas complète. Veuillez la compléter.',
+                'actif' => '1',
+                'date' => '2021-01-01',
+                'idEmploye' => 1,
+            ],
+
+            [
+                'type' => '0',
+                'conducteur_id' => 1,
+                'message' => 'Vous n\'avez pas eu au moins 24 heures de repos d\'affilée au cours des 14 derniers jours, veuillez contacter votre contremaître.',
+                'actif' => '1',
+                'date' => '2021-01-01',
+                'idEmploye' => 0,
+            ],
+
+            [
+                'type' => '0',
+                'conducteur_id' => 1,
+                'message' => 'Le conducteur Jean Buteau n\'a pas eu au moins 24 heures de repos d\'affilée au cours des 14 derniers jours.',
+                'actif' => '1',
+                'date' => '2021-01-01',
+                'idEmploye' => 1,
+            ],
+
         ]);
     }
 }
